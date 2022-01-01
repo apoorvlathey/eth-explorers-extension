06e39e0fca5c79a8167a8ad20ee049b603a21058
CodinMaster <latheyapoorv@gmail.com>
2022-01-02 00:41:06 +0530

    init

---
+++ eventPage.js
@@ -0,0 +1,50 @@
+const explorersInfo = {
+  Etherscan: {
+    baseUrl: "https://etherscan.io/tx/",
+  },
+  Bloxy: {
+    baseUrl: "https://bloxy.info/tx/",
+  },
+  "Ethtxn.info": {
+    baseUrl: "https://ethtx.info/mainnet/",
+  },
+  Tenderly: {
+    baseUrl: "https://dashboard.tenderly.co/tx/mainnet/",
+  },
+};
+
+const explorerIds = [];
+
+for (var key in explorersInfo) {
+  if (explorersInfo.hasOwnProperty(key)) {
+    explorerIds.push(key);
+  }
+}
+
+for (var i = 0; i < explorerIds.length; i++) {
+  chrome.contextMenus.create({
+    id: explorerIds[i],
+    title: explorerIds[i],
+    contexts: ["selection"],
+  });
+}
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+  if (
+    explorersInfo.hasOwnProperty(clickData.menuItemId) &&
+    clickData.selectionText
+  ) {
+    if (!validateTxnHash(clickData.selectionText)) {
+      alert("Txn Hash is invalid!");
+      return;
+    }
+
+    var newURL =
+      explorersInfo[clickData.menuItemId].baseUrl + clickData.selectionText;
+    chrome.tabs.create({ url: newURL });
+  }
+});
+
+const validateTxnHash = (str) => {
+  return /^0x([A-Fa-f0-9]{64})$/.test(str);
+};
